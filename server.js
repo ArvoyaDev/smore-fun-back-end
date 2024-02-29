0cf4711073b61bde1451f5cab594cead4ac9c25b
Isai Chaidez <isaichaidez@gmail.com>
2024-02-29 13:02:15 -0800

    added cache support to requests can persist while server is on

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Note = require('./models/notes');
 const {readNotes, postNotes, putNotes, deleteNotes} = require('./handlers');
 const cityInfoRouter = require('./lib/router');
 
